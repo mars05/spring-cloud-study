5f1b5da2af0ac06e0ff96c9a842c86f71431b224
xiaoyu <xiaoyucoder@163.com>
2018-11-19 15:40:24 +0800

    init

---
+++ m5-cloud-config/m5-cloud-config-client/src/main/java/com/github/m5/cloud/config/client/M5CloudConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.github.m5.cloud.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M5CloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M5CloudConfigClientApplication.class, args);
+	}
+}
---
+++ m5-cloud-config/m5-cloud-config-server/src/main/java/com/github/m5/cloud/config/server/M5CloudConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.github.m5.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author xiaoyu
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class M5CloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M5CloudConfigServerApplication.class, args);
+    }
+}
